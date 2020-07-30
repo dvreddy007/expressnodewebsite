3dbbb42aaadcb8ee30894a450acbacb5ad078f78
Venkat <dvreddy007@gmail.com>
2020-07-30 19:03:01 +0530

    pushing the changes to dev

--- app.js
+++ app.js
@@ -9,28 +9,72 @@ const router = express.Router();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
+app.set('view engine', 'ejs')
 
-let connectionString = 'mongodb+srv://venkat1:venkat1@cluster0.vgw4f.mongodb.net/<dbname>?retryWrites=true&w=majority';
+let connectionString = 'mongodb+srv://venkat1:venkat1@cluster0.vgw4f.mongodb.net/testdb?retryWrites=true&w=majority';
 
 
 // adding mongo client to get connected to mango db
-
-MongoClient.connect(connectionString, (err, client) => {
-  // ... do something here
-  if (err) return console.error(err);
+/*
+const uri = "mongodb+srv://venkat1:venkat1@cluster0.cn8fi.mongodb.net/testdb?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 1000
+}, { useNewUrlParser: true,
+    useUnifiedTopology: true, });
+client.connect(err => {
+  let collection = client.db("gqrsdb").collection("userdetails");
   console.log('Connected to Database');
-  var db = client.db('testdb')
-  var userCollection = db.collection('userdetails')
-	 app.post('/userdetails', (req, res) => {
-	  console.log('user details are ....'); 
-	  console.log(req.body);
-	  userCollection.insertOne(req.body)
-		.then(result => {
-		  console.log(result)
-		  res.redirect('/userform');
+  // perform actions on the collection object
+		app.post('/userdetails', (req, res) => {
+		  console.log('user details are ....'); 
+		  console.log(req.body);
+		  collection.insertOne(req.body)
+			.then(result => {
+			  console.log(result)
+			  // res.redirect('/userform');
+			  // res.render('/views/userform.ejs', { userdetails: results })
+			  
+			})
+			.catch(error => console.error(error))
+		});
+		app.get('/userdetails', (req, res) => {
+		  client.db("gqrsdb").collection('userdetails').find().toArray()
+			.then(results => {
+				console.log(JSON.stringify(userdetails));
+			  res.render('/views/userform.ejs', { userdetails: results })
+			})
+			.catch()
 		})
-		.catch(error => console.error(error))
-	});
+  client.close();
+});
+*/
+MongoClient.connect(connectionString, (err, client) => {
+  if (err) return console.error(err)
+  console.log('Connected to Database')
+	var db = client.db('testdb')
+	  var userCollection = db.collection('userdetails')
+		 app.post('/userdetails', (req, res) => {
+		  console.log('user details are ....'); 
+		  console.log(req.body);
+		  userCollection.insertOne(req.body)
+			.then(result => {
+			  console.log(result)
+			  res.redirect('/views/userform');
+			  //res.render('/views/userform.ejs', { userdetails: results })
+			  
+			})
+			.catch(error => console.error(error))
+		});
+		
+		app.get('/views/userform', (req, res) => {
+		  db.collection('userdetails').find().toArray()
+			.then(results => {
+				console.log(results);
+			  res.render('userform.ejs', { userdetails: results })
+			})
+			.catch()
+		})
 })
 
 
@@ -42,6 +86,10 @@ router.get('/',function(req,res){
   //__dirname : It will resolve to your project folder.
 });
 
+router.get('/userform',function(req,res){
+  res.sendFile(path.join(__dirname+'/userform.html'));
+  //__dirname : It will resolve to your project folder.
+});
 
 router.get('/userform',function(req,res){
   res.sendFile(path.join(__dirname+'/userform.html'));
